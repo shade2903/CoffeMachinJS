ebef54f26d32d0051a58ae33245b850d6684040a
shade2903 <nighshade2903@gmail.com>
2021-09-10 12:36:38 +0300

    version 1.1

--- script.js
+++ script.js
@@ -15,26 +15,33 @@ class CoffeMachin{
         }else{
             this.water -=40;
             this.coffee -=10;
-        }
-             
+        }  
          
         
     }
     doubleEspresso(){
-        this.espresso();
-        this.espresso();
+        if(this.water -80 <=0){
+            this.notEnoughWater();
+        }else if(this.coffee - 20 <=0){
+            this.notEnoughCoffee();
+        }else{
+            this.water -=80;
+            this.coffee -=20;
+        }    
         
     }
     americano(){
-        this.doubleEspresso();
-        if(this.water - 120 <=0){
-            this.water -=120;
-        }            
-        
+        if(this.water -200 <=0){
+            this.notEnoughWater();
+        }else if(this.coffee - 20 <=0){
+            this.notEnoughCoffee();
+        }else{
+            this.water -=200;
+            this.coffee -=20;
+        }     
     }
     fillWater(){
-        this.water = maxWater;
-        
+        this.water = maxWater;        
     }
     fillCoffee(){
         this.coffee = maxCoffe;
@@ -43,24 +50,29 @@ class CoffeMachin{
         alert("Не хватает воды! Заполните бак водой.")
     }
     notEnoughCoffee(){
-        alert("Не хватает кофе! Заполните кофейный контейнер")
+        alert("Не хватает кофе! Заполните кофейный контейнер.")
     }
-
 }
  var coffeMachin = new CoffeMachin(maxWater,maxCoffe);
  let iWater = document.querySelector('.amountOfWater');
  let iCoffe = document.querySelector('.amountOfCoffee');
  let diagram1 = document.getElementById('diagram1');
  let diagram2 = document.getElementById('diagram2');
- 
 
 
- addEventListener('click',() =>{ //button
+ addEventListener('click',() =>{ 
     iWater.textContent = coffeMachin.water + " ml";
     iCoffe.textContent = coffeMachin.coffee + " g";
     diagram1.style.height = coffeMachin.water/maxWater*100 + "%";
     diagram2.style.height = coffeMachin.coffee/maxCoffe*100 + "%";
+    diagram1.textContent = Math.round((coffeMachin.water/maxWater*100)) + "%";
+    diagram2.textContent = Math.round((coffeMachin.coffee/maxCoffe*100)) + "%";
  });
+ const header = document.getElementById("header")
+ header.style.backgroundImage = "url('https://img.freepik.com/free-photo/coffee-beans-top-view-on-a-white-background-space-for-text_176474-5028.jpg?size=626&ext=jpg')"
+ header.style.backgroundSize = "100%";
+
  
+
 
 
